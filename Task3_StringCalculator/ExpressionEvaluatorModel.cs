3823a27eadf5ddfb79cf4600c348fa23154e5a1f
Lisa Lobo <lisalobo22@gmail.com>
2023-11-26 14:11:51 +0200

    Created ExpressionEvaluatiorModel class

---
+++ Task3_StringCalculator/ExpressionEvaluatorModel.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_ExpressionEvaluator
+{
+    public class ExpressionEvaluatorModel
+    {
+
+        public string ExpressionLeft { get; set; }
+        public string ExpressionRight { get; set; }
+        public string FoundDelimiter { get; set; }
+
+        public string MultiValue { get; set; }
+
+        public Stack<float> Operands { get; set; }
+        public Stack<char> Operators { get; set; }
+        public Stack<float> ExpressionResults { get; set; }
+
+        public Dictionary<char, int> Precedence { get; set; }
+
+        // Constructor to initialize stacks, dictionary, etc.
+        public ExpressionEvaluatorModel()
+        {
+            Operands = new Stack<float>();
+            Operators = new Stack<char>();
+            ExpressionResults = new Stack<float>();
+
+            Precedence = new Dictionary<char, int>
+        {
+            {'+', 1 },
+            {'-', 1 },
+            {'*', 2 },
+            {'/', 2 }
+        };
+        }
+
+        void expressionOrganizer(string expression)
+        {
+            string[] delimiters = { ">=", "<=", "==", "=", "<", ">" };
+            FoundDelimiter = delimiters.FirstOrDefault(d => expression.Contains(d));
+
+            if (!string.IsNullOrEmpty(FoundDelimiter))
+            {
+                string[] splitArray = expression.Split(new string[] { FoundDelimiter }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (splitArray.Length == 2)
+                {
+                    ExpressionLeft = splitArray[0];
+                    ExpressionRight = splitArray[1];
+
+
+                }
+                else
+                {
+                    throw new Exception("Invalid input: String does not contain exactly one delimiter");
+                }
+            }
+
+            else
+            {
+                throw new Exception("No valid delimiter found in the string");
+            }
+        }
+
+
+        void Evaluator(string expression)
+        {
+            foreach (char c in expression)
+            {
+
+                if (char.IsDigit(c) || c == '.')
+                {
+                    MultiValue += c;
+
+                    continue;
+                }
+                else if (c == '(')
+                {
+                    Operators.Push(c);
+                }
+                if (c == ')')
+                {
+                    while (Operators.Count > 0 && Operators.Peek() != '(')
+                    {
+                        if (MultiValue.Length > 0)
+                        {
+                            Operands.Push(float.Parse(MultiValue));
+                            MultiValue = "";
+                        }
+
+                        ProcessOperations(Operands, Operators);
+                    }
+
+                }
+                else if (Precedence.ContainsKey(c))
+                {
+                    if (Operators.Count > 0 && Operators.Peek() == '(')
+                    {
+                        Operators.Pop(); // Discard the '('
+                    }
+                    while (Operators.Count > 0 && Precedence[c] <= Precedence[Operators.Peek()])
+                    {
+                        if (MultiValue.Length > 0)
+                        {
+                            Operands.Push(float.Parse(MultiValue));
+                            MultiValue = "";
+                        }
+                        ProcessOperations(Operands, Operators);
+                    }
+
+                    Operators.Push(c);
+                }
+                else if (MultiValue.Length > 0)
+                {
+                    Operands.Push(float.Parse(MultiValue));
+                    MultiValue = "";
+                }
+            }
+            if (MultiValue.Length > 0)
+            {
+                Operands.Push(float.Parse(MultiValue));
+                MultiValue = "";
+            }
+            if (Operators.Count > 0)
+            {
+                ProcessOperations(Operands, Operators);
+            }
+            ExpressionResults.Push(Operands.Pop());
+        }
+
+        bool ExpressionComparer()
+        {
+            string delimiter = FoundDelimiter;
+            float expressionResultRight = ExpressionResults.Pop();
+            float expressionResultLeft = ExpressionResults.Pop();
+            bool result = false;
+
+            switch (delimiter)
+            {
+                case "=":
+                    if (expressionResultLeft == expressionResultRight)
+                    {
+                        result = true;
+                    }
+                    break;
+
+                case "<":
+                    if (expressionResultLeft < expressionResultRight)
+                    {
+                        result = true;
+                    }
+                    break;
+
+                case ">":
+                    if (expressionResultLeft > expressionResultRight)
+                    {
+                        result = true;
+                    }
+                    break;
+
+                case "==":
+                    if (expressionResultLeft == expressionResultRight)
+                    {
+                        result = true;
+                    }
+                    break;
+
+                case "<=":
+                    if (expressionResultLeft <= expressionResultRight)
+                    {
+                        result = true;
+                    }
+                    break;
+
+                case ">=":
+                    if (expressionResultLeft >= expressionResultRight)
+                    {
+                        result = true;
+                    }
+                    break;
+
+                default:
+
+                    throw new Exception();
+
+
+            }
+            return result;
+        }
+
+        static void ProcessOperations(Stack<float> operands, Stack<char> operators)
+        {
+            char operatorSymbol = operators.Pop();
+            float operand1 = operands.Pop();
+            float operand2 = operands.Pop();
+            float result = 0;
+
+            switch (operatorSymbol)
+            {
+                case '+':
+                    result = operand1 + operand2;
+                    break;
+
+                case '-':
+                    result = operand1 - operand2;
+                    break;
+
+                case '*':
+                    result = operand1 * operand2;
+                    break;
+
+                case '/':
+                    result = operand2 / operand1;
+                    break;
+                default:
+                    throw new Exception();
+            }
+            operands.Push(result);
+
+
+
+        }
+
+    }
+
+
+}
